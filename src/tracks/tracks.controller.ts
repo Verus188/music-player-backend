bd63d00ca281edb508d4fee1b32ffe0b8390ba1a
Nikita <dudinnikita03188@gmail.com>
2026-01-13 23:39:06 +0300

    [feat] - добавил описание эндпоинтам в сваггере

--- src/tracks/tracks.controller.ts
+++ src/tracks/tracks.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Post, Body } from '@nestjs/common';
 import { TracksService } from './tracks.service';
 import { CreateTrackDto } from './dto/create-track.dto';
 import { StorageService } from 'src/storage/storage.service';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('tracks')
 export class TracksController {
@@ -12,11 +13,19 @@ export class TracksController {
 
   // TODO: запретить создавать треки через эндпоинт, когда будет api
   @Post()
+  @ApiOperation({
+    summary: 'Загрузка трека в бд',
+    description: 'Сейчас нужна для тестов. В будущем будет удалена',
+  })
   create(@Body() createTrackDto: CreateTrackDto) {
     return this.tracksService.create(createTrackDto);
   }
 
   @Get('test-upload')
+  @ApiOperation({
+    summary: 'Загрузка тестового файла в хранилище',
+    description: 'Будет удален в будущем',
+  })
   async testUpload() {
     const buffer = Buffer.from('Hello, World!', 'utf-8');
     const fileName = `test-${Date.now()}.txt`;
