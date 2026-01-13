6f803710b2305e6f8b6ca2310ef05cee6ae5a3b0
Nikita <dudinnikita03188@gmail.com>
2026-01-13 22:46:19 +0300

    [feat] - создан сваггер

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Music player API')
+    .setDescription('API для музыкального плеера')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, swaggerDocument);
+
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.enableCors({
     origin: '*', // TODO: изменить на конкретный домен в продакшене
